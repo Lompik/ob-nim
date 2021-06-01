7c5208eab5d921bdd05d294d92176648bf5245f5
Kaushal Modi <kaushal.modi@gmail.com>
2021-06-01 10:00:47 -0400

    Do not try to convert results to numbers
    Below is tested on Org 9.4.6-544-gc5573b
    
    Before this commit:
    
        #+begin_src nim
        echo "-0"
        #+end_src
        #+RESULTS:
        : 0
    
    After this commit:
    
        #+begin_src nim
        echo "-0"
        #+end_src
        #+RESULTS:
        : -0
    
    The change in this commit matches what ob-python.el does.

--- ob-nim.el
+++ ob-nim.el
@@ -111,16 +111,11 @@ header arguments."
 	        "")))
       (when results
 	    (org-babel-reassemble-table
-	     (org-babel-result-cond (cdr (assoc :result-params params))
-	       (org-babel-read results t)
-	       (let ((tmp-file (org-babel-temp-file "c-")))
-	         (with-temp-file tmp-file (insert results))
-	         (org-babel-import-elisp-from-file tmp-file)))
+	     results
 	     (org-babel-pick-name
 	      (cdr (assoc :colname-names params)) (cdr (assoc :colnames params)))
 	     (org-babel-pick-name
-	      (cdr (assoc :rowname-names params)) (cdr (assoc :rownames params)))))
-      )))
+	      (cdr (assoc :rowname-names params)) (cdr (assoc :rownames params))))))))
 
 (defun org-babel-nim-expand-nim (body params)
   "Expand a block of nim code with org-babel according to
